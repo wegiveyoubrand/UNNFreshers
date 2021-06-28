e35ae32fbfc72a48150e5606037e0785375b99c5
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-06-28 11:00:45 +0100

    Updating Dashboard UI

--- dashboard/src/js/user.js
+++ dashboard/src/js/user.js
@@ -1,7 +1,18 @@
+
 const openUser = document.querySelector(".avatar");
 
 const userOptions = document.querySelector(".user_options");
+const userLogout = document.querySelector("#user_logout");
+
+// const usernameContainer = document.querySelector(".userName_container");
+
+const usernameContainer = document.querySelector(".userName_container");
 
 openUser.addEventListener("click", () => {
   userOptions.classList.toggle("open");
 });
+
+userLogout.addEventListener("click", () => {
+  location.href = "/index.html";
+});
+
---
+++ src/js/user.js
@@ -0,0 +1,9 @@
+const submitForm = document.querySelector("#submitForm");
+const username_cc = document.querySelector('#username')
+
+
+
+const userName = "John";
+
+const usernameContainer = document.querySelector("#userName_container");
+usernameContainer.textContent = userName
