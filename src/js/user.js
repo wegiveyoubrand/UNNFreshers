3688a700142c7ff57a72a70d73dbe37f9bea1409
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-06-29 10:37:10 +0100

    updated

--- src/js/user.js
+++ src/js/user.js
@@ -1,10 +1,14 @@
 const username_cc = document.querySelector("#username");
 
-function submitForm() {
-  location.href = "./dashboard.html";
-}
+let form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  location.href = "./dashboard/index.html";
+});
+
+function submitForm() {}
 
 const userName = "Kosisochukwu";
 
 const usernameContainer = document.querySelector("#userName_container");
-usernameContainer.textContent = userName;
+// usernameContainer.textContent = userName;
