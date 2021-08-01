20f9065288446a6efc8b3eb3c3df314472d03acf
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-08-01 15:57:10 +0100

    Updated

--- dashboard/src/js/modules/Modal.js
+++ dashboard/src/js/modules/Modal.js
@@ -1,23 +1,40 @@
-const services = document.querySelectorAll(".portal");
+const Modal = document.querySelector(".modal"),
+  ModalComponent = document.querySelector("modal-component"),
+  modalContent = document.querySelector(".modal-content"),
+  ModalClose = document.querySelector(".modal-close"),
+  main = document.querySelector("main"),
+  services = document.querySelectorAll(".portal");
+
+ModalClose.addEventListener("click", (e) => {
+  e.preventDefault;
+  Modal.classList.remove("open");
+  main.classList.remove("fixed");
+});
 
 services.forEach((service) => {
   service.addEventListener("click", (e) => {
     e.preventDefault;
-    console.log(service.getAttribute("data-name"));
-    let dataAttribute = service.getAttribute("data-name");
+    Modal.classList.add("open");
+    main.classList.add("fixed");
+    if (service.getAttribute("data-name") !== null) {
+      console.log(service.getAttribute("data-name"));
+      let dataAttribute = service.getAttribute("data-name"),
+        lowerData;
 
-    if (dataAttribute === "") {
-      alert("error");
-    } else {
-      let lowerData = dataAttribute.toLowerCase();
+      if (dataAttribute !== "") {
+        lowerData = dataAttribute.toLowerCase();
 
-      if (lowerData == "computer services") {
-        alert("ok");
-      } else if (lowerData == "online payment") {
-        alert("online payment");
-      } else {
-        alert("error");
+        if (lowerData == "computer services") {
+          modalContent.innerHTML = ComputerServicesContent;
+        } else if (lowerData == "online payment") {
+          alert("online payment");
+        } else {
+          alert("error");
+        }
       }
     }
   });
 });
+
+// Set Modal Content
+const ComputerServicesContent = `Hello Computer Services`;
